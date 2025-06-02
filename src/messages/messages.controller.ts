20641f5802cc60d14748cbab1650bc973c55f9a7
joaovrmoura <joaovrmoura4@gmail.com>
2025-06-01 23:47:52 -0300

    HTTP decorators

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,16 +1,43 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 
+// Patch is used to update a data of a recurse
+// Put is used for update entire data of a recurse
+
+// crud of messages
 @Controller('messages')
 export class MessagesController {
   // find all messages
+  @HttpCode(HttpStatus.OK)
   @Get()
   findAll(): string {
     return 'Return all messages';
   }
   // get a route parameter
   @Get(':id')
-  findOne(@Param() id: any) {
+  findOne(@Param('id') id: string): string {
     console.log(id);
-    return `Route Parameter of message`;
+    return `Route Parameter of message ${id}`;
+  }
+  // POST for Create
+  @Post()
+  create(@Body() body: any): any {
+    return body;
+  }
+  // update a message
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body: string) {
+    return {
+      id,
+      body,
+    };
   }
 }
